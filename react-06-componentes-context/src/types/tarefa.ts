f5e62e7eac22a0d874c8d3114a90b6f07f31fd5d
Guilherme2344 <guilhermecsilva2.alternative@gmail.com>
2025-06-04 13:23:35 -0300

    update

--- react-06-componentes-context/src/types/tarefa.ts
+++
@@ -1,8 +0,0 @@
-export interface Tarefa {
-    id: number;
-    todo: string; // A descrição da tarefa
-    completed: boolean; // Status se a tarefa foi completada
-    userId: number; // ID do usuário associado à tarefa
-}
-  
-export type NovaTarefa = Pick<Tarefa, 'todo' | 'completed' | 'userId'>;
